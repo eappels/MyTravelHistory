517400ffe0921d856edb0ecc45ba72e6bb17f98f
appels <appels@telenet.be>
2025-06-24 02:49:09 +0200

    Dev view added

--- MyTravelHistoryApp/ViewModels/DevViewModel.cs
+++ MyTravelHistoryApp/ViewModels/DevViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MyTravelHistoryApp.Services.Interfaces;
+using System.Diagnostics;
 
 namespace MyTravelHistoryApp.ViewModels;
 
@@ -12,11 +13,24 @@ public partial class DevViewModel : ObservableObject
     public DevViewModel(IDBService dbService)
     {
         this.dbService = dbService;
+        MainThread.BeginInvokeOnMainThread(async () =>
+        {
+            var pausesLocationUpdatesAutomatically = await SecureStorage.Default.GetAsync("PausesLocationUpdatesAutomatically");
+            PausesLocationUpdatesAutomaticallyIsToggled = pausesLocationUpdatesAutomatically == "True";
+        });
     }
 
     [RelayCommand]
     private async void ResetDatabase()
     {
         await dbService.ClearDatabase();
+    }
+
+    [ObservableProperty]    
+    private bool pausesLocationUpdatesAutomaticallyIsToggled;
+
+    partial void OnPausesLocationUpdatesAutomaticallyIsToggledChanged(bool value)
+    {
+        Debug.WriteLine($"PausesLocationUpdatesAutomatically is set to {value}.");
     }
 }
